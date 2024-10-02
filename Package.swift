7346a6cb3ae3871494db41bce798f42459459b5a
Péter Krassay <peter.krassay@proton.me>
2024-10-01 19:30:30 -0600

    Improve framework output

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
@@ -14,17 +12,20 @@ let package = Package(
             targets: ["NFCPassportReader"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "1.1.2200"))
 
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NFCPassportReader",
-            dependencies: ["OpenSSL"]),
+            dependencies: ["OpenSSL"],
+            swiftSettings: [
+                .unsafeFlags([
+                    "-emit-module-interface",
+                    "-enable-library-evolution"
+                ])
+            ]
+        ),
     ]
 )
 
